dee8bd92c44102b57e8e5ab7812b1b92152743e0
sowmya333-gif <sowmyaky1234@gmail.com>
2020-06-28 23:31:00 +0530

    first commit

---
+++ src/com/githubproject/pgm/Santhu.java
@@ -0,0 +1,12 @@
+package com.githubproject.pgm;
+
+public class Santhu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welecome to my first github project");
+		
+		
+	}
+	
+}
